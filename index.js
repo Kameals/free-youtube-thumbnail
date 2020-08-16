0c037f98cd8dca73185d34fc8daec6109d1e56e9
Hyomin Kim <h.kim@gvatech.co.jp>
2020-08-16 18:45:13 +0900

    delete express

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const helmet = require('helmet');
 app.use(express.static('public'));
 app.use(helmet());
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
 const port = process.env.PORT || 4243;
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
